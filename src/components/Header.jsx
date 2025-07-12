0b962fdd35d0712d1535e715143aa251f6444537
Shubham Yadav <shub252005@gmail.com>
2025-07-12 14:57:40 +0530

    IP_task

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-
+import React from 'react';
+import './Header.css';
 const Header = () => {
+  // function changeText() {
+  //   let x = 5;
+  //   document.getElementById("result").innerText = `square of ${x} is ${x * x}`;
+  // }
+  let url="https://jsonplaceholder.typicode.com/users";
+  fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      console.table(result);
+      
+      let output=""
+      for(let i=0; i<result.length; i++){
+        output += `<tr>
+          <td>${result[i].id}</td>
+          <td>${result[i].name}</td>
+          <td>${result[i].email}</td>
+        </tr>`;
+      }
+      
+      document.getElementById("table").innerHTML = output;
+    })
+
   return (
-    <div>
-      <h1 className=' flex text-white justify-center items-center font-extrabold text-2xl bg-violet-800 h-14 '>The Quiz Marathon</h1>
-    </div>
-  )
-}
+    <>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody id ="table"></tbody>
+      </table>
+      {/* <div id="result">I am from dev</div>
+      <button onClick={changeText}>Click me</button> */}
+
+    </>
+  );
+};
 
 export default Header
